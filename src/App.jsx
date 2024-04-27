267e7034c192491c2dd0e8ffa502eead132df7a1
Scott Harvey <Scott Harvey@harvey41574@gmail.com>
2024-04-27 13:59:48 -0400

    PuppyPals 2nd

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import { puppyList } from "./data"
+
+
+function App() {
+  const [puppies, setPuppies]= useState(puppyList);
+  const [featPupId, setFeatPupId]= useState(null);
+
+function handleClick(){
+
+}
+
+
+ const featuredPup= puppies.find((pup)=>pup.id===featPupId)
+ console.log(featuredPup)
+   {featPupId && (
+      <div> 
+        <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age:{featuredPup.age}</li>
+            <li>Email:{featuredPup.email}</li>
+         </ul> 
+      </div>
+   )}
+  return (
+    <>
+      <div className= "App">
+      
+        
+          {puppies.map((puppy)=>{
+            return(
+            <p onClick={()=>{setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
+            
+      
+                        );  
+                    
+               })}
+              
+
+              
+              </div> 
+                
+    </>
+  )
+}
+
+export default App
